07f4f820c43d4f35f1da810fe15ffe38c4d82cf5
Laura Tateosian <lgtateos@ncsu.edu>
2018-08-24 11:23:11 -0400

    Add files via upload

---
+++ example_scripts/add.py
@@ -0,0 +1,15 @@
+# add.py adds two numbers given as arguments
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = a + b
+print "The sum is", c
+
+
+####To uncomment a block, select it, select "source code" and "uncomment block" or Alt + 4
+####To comment a block, select and press Alt + 3
+##print "sys.argv[1] + sys.argv[2] = ", sys.argv[1] + sys.argv[2]
+##print "sys.argv[0] = ", sys.argv[0]
+##print "sys.argv[1] = ", sys.argv[1]
+##print "sys.argv[2] = ", sys.argv[2]
