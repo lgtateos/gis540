4c04dcbd808669e83b7c1382c83b9d2495a874bd
Laura Gray Tateosian <lgtateos@wolftech.ad.ncsu.edu>
2020-01-23 16:27:15 -0500

    Added parentheses to print statements

--- example_scripts/add.py
+++ example_scripts/add.py
@@ -4,12 +4,12 @@ import sys
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = a + b
-print "The sum is", c
+print ( "The sum is", c )
 
 
 ####To uncomment a block, select it, select "source code" and "uncomment block" or Alt + 4
 ####To comment a block, select and press Alt + 3
-##print "sys.argv[1] + sys.argv[2] = ", sys.argv[1] + sys.argv[2]
-##print "sys.argv[0] = ", sys.argv[0]
-##print "sys.argv[1] = ", sys.argv[1]
-##print "sys.argv[2] = ", sys.argv[2]
+##print( "sys.argv[1] + sys.argv[2] = ", sys.argv[1] + sys.argv[2] )
+##print( "sys.argv[0] = ", sys.argv[0] )
+##print( "sys.argv[1] = ", sys.argv[1] )
+##print( "sys.argv[2] = ", sys.argv[2] )
