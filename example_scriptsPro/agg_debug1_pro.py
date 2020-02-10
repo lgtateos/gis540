ce369d0395116065df601e4923fed01d7d66cc7f
cnich89 <51102613+cnich89@users.noreply.github.com>
2020-02-09 21:45:36 -0500

    Add files via upload

---
+++ example_scriptsPro/agg_debug1_pro.py
@@ -0,0 +1,12 @@
+import arcpy
+
+arcpy.env.workspace = "C:/gispy/scratch"
+
+for numDist in range(100,200,1000):
+    # Do Geoprocessing Stuff
+    arcpy.env.overwriteOutput = 1
+    input = "C:/gispy/data/ch10/park.shp"
+    aggreDist = str(numDist), "meter"
+    aggreOut = input.split(".")[0] + str(numDist)+ "agg.shp"
+    arcpy.AggregatePolygons(input, aggreOut, aggreDist)
+    print(aggreOut)
