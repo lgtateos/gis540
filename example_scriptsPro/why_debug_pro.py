0e74b6293d59ebf82d278eb83759dc3119a4d439
cnich89 <51102613+cnich89@users.noreply.github.com>
2020-02-09 21:38:14 -0500

    Add files via upload

---
+++ example_scriptsPro/why_debug_pro.py
@@ -0,0 +1,40 @@
+#Name: basin_process_tool.py
+#
+#Description: written to process 10 and 12 digit HUCs for the DWQ project
+#
+#Requirements: Be cool, man.  Be cool.
+
+# Import system modules
+import sys, string, os
+
+#input directory with KML files
+InKMLSpace = "C:\\temp"
+
+#set directory with corresponding geospacial information (spapefiles or feature classes...)
+GeoDirectory = "C:\\temp"
+
+#Get a list of files in the InKMLSpace
+FileList = os.listdir(InKMLSpace)
+
+for file in FileList:
+
+    if file.endswith("kml"):
+        #open KML document
+        #set variables for conditional statement that checks geospatial file type
+        shapefile = GeoDirectory + "\\" + file[:-4] + ".shp"
+        featureClass = GeoDirectory + "\\" + file[:-4]
+
+        #check to see if corresponding geospatial file is a shapefile or a feature class
+        if os.path.exists(shapefile):
+            featurerLayer = shapefile
+            print("shapefile")
+        elif os.path.exists(featureClass):
+            featureLayer = featureClass
+            print("feature class")
+        else:
+            featureLayer = "no matching files."
+
+        print(featureLayer)
+
+    else:
+        print(file + " is not a KML file, skipping.")
