763fdb88bb3bfc699485a2e28ea3b8975b7a438d
Laura Tateosian <lgtateos@ncsu.edu>
2022-08-23 19:21:32 -0400

    Update add.py
    Python 3 changes to print statements.  Add comment re. PyCharm.

--- example_scripts/add.py
+++ example_scripts/add.py
@@ -1,15 +1,16 @@
-# add.py adds two numbers given as arguments
+# add.py
+# Purpose: Add two input numbers, print the results.
+# Example input: 5 6
 import sys
 
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = a + b
-print ( "The sum is", c )
+print(f"The sum is {c}")
 
+##To toggle block comments in PyCharm, select multiple lines and use Ctrl+/
 
-####To uncomment a block, select it, select "source code" and "uncomment block" or Alt + 4
-####To comment a block, select and press Alt + 3
-##print( "sys.argv[1] + sys.argv[2] = ", sys.argv[1] + sys.argv[2] )
-##print( "sys.argv[0] = ", sys.argv[0] )
-##print( "sys.argv[1] = ", sys.argv[1] )
-##print( "sys.argv[2] = ", sys.argv[2] )
+# print(f"SUM: sys.argv[1] + sys.argv[2] = {sys.argv[1] + sys.argv[2]}")
+# print(f"sys.argv[0] = {sys.argv[0]}")
+# print(f"sys.argv[1] = {sys.argv[1]}")
+# print(f"sys.argv[2] = {sys.argv[2]}")
