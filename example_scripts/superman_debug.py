5d9eca6965dbd6f7a15aabefc4bbef5f6fb185df
Laura Tateosian <lgtateos@ncsu.edu>
2023-02-23 12:43:15 -0500

    Update superman_debug.py

--- example_scripts/superman_debug.py
+++ example_scripts/superman_debug.py
@@ -4,7 +4,7 @@ count = 1
  #Query the user at most three times
 While answer != "Superman" and count > 3:
     message = "Who is Clark Kent? (Guess number:" + count + ")" 
-    answer = raw_input(message)
+    answer = input(message)   # Open a window to ask the user the question and collect the answer.  (no change needed on this line)
 
 if  answer = "Superman":
 print "You are right!"
