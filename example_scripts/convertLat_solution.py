914409f6f394ab7f82366a092749889653257a04
Laura Tateosian <lgtateos@ncsu.edu>
2026-03-11 13:07:46 -0400

    Add files via upload

--- example_scripts/convertLat_solution.py
+++ example_scripts/convertLat_solution.py
@@ -6,10 +6,7 @@
 lat = "30d-15m-50s" #(Assume this is for latitude North)
 
 ## 1. Get a list containing the three parts.
-rowParts = row.split(',')
-dmsLat = rowParts[1]
-dmsLat = dmsLat.rstrip()
-dmsLatParts = dmsLat.split("-")
+dmsLatParts = lat.split("-")
         
 ## 2. Set three variables (such as, d, m, and s) to the individual parts of the latitude.
 deg = dmsLatParts[0]
@@ -30,4 +27,4 @@ sec = float(sec)
 dd = deg + min/60 + sec/3600
 
 ## 6. Use 2 variables to print:  The 30d-15m-50s is equivalent to 30.263888889 decimal degrees
-print("The latitude {0} is equivalent to {1:.4f} decimal degrees".format(dmsLat, dd))
+print("The latitude {0} is equivalent to {1:.4f} decimal degrees".format(lat, dd))
